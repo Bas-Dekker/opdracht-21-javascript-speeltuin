67a91c75d90af13ce3b70e57d803989a55d3adcd
Bas <bmjdekker@gmail.com>
2021-03-02 13:27:57 +0100

    Add files via upload

---
+++ Opdracht eerste JavaScript/script.js
@@ -0,0 +1,14 @@
+console.log("Hello Winc Academy");
+
+let firstName = "Bas";
+console.log(firstName);
+
+console.log(1 + 1);
+
+console.log("1" + "1");
+
+let age = "42";
+console.log(typeof age);
+
+let ageOne = "tweeenveertig";
+console.log(typeof ageOne);
